c0f05ed899ba132fb47fd04828c47293df47da6b
Noah <Nore5515@gmail.com>
2019-04-25 19:10:57 -0500

    Finished import/export

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -29,7 +29,8 @@ namespace ProDerSquads
             while (!playing)
             {
                 Console.WriteLine("What would you like to do?\n\t'dmg' - Calculate damage dealt of a Squad\n\t'buildsquad' - Builds your Squad");
-                Console.WriteLine("\t'view' - View your current squad");
+                Console.WriteLine("\t'view' - View your current squad\n\t'full view' - View your squad in it's entirety!\n\t'export' - Displays your squad code.");
+                Console.WriteLine("\t'import' - Import a squad from a chain of text");
                 reply = Console.ReadLine();
                 if (reply.Equals("dmg"))
                 {
@@ -43,6 +44,81 @@ namespace ProDerSquads
                 {
                     Console.WriteLine(main.view());
                 }
+                else if (reply.Equals("full view"))
+                {
+                    for (int x = 0; x < main.squad.Length; x++)
+                    {
+                        Console.WriteLine(main.squad[x].ToString());
+                    }
+                }
+                else if (reply.Equals("export"))
+                {
+                    Console.WriteLine(main.export());
+                }
+                else if (reply.Equals("import"))
+                {
+                    Console.WriteLine("Copy your Code Here:");
+                    reply = Console.ReadLine();
+                    string manip;
+                    bool importing = true;
+                    int count = 0;
+                    Soldier s;
+                    while (importing)
+                    {
+                        if (reply.Length < 3)
+                        {
+                            importing = false;
+                            break;
+                        }
+                        s = new Soldier();
+                        manip = reply.Substring(0,reply.IndexOf("|"));
+                        if (manip.Substring(0, 1).Equals("0"))
+                        {
+                            s.setSpecies("Derse");
+                        }
+                        else if (manip.Substring(0,1).Equals("1"))
+                        {
+                            s.setSpecies("Prospit");
+                        }
+                        else
+                        {
+                            Console.WriteLine("ERR IN SPECIES SET - IMPORT");
+                        }
+
+                        manip = manip.Substring(1, manip.Length - 1);
+
+                        if (manip.Substring(0, 1).Equals("0"))
+                        {
+                            s.setType("Conscript");
+                        }
+                        else if (manip.Substring(0, 1).Equals("1"))
+                        {
+                            s.setType("Basic Infantry");
+                        }
+                        else
+                        {
+                            Console.WriteLine("ERR IN TYPE SET - IMPORT");
+                        }
+
+                        manip = manip.Substring(1, manip.Length - 1);
+                        //Console.WriteLine(manip);
+                        //Console.WriteLine($"-{manip.IndexOf("-")}-\t-{manip.Length - manip.IndexOf("-") - 1}-");
+                        manip = manip.Substring(manip.IndexOf("-")+1, manip.Length - manip.IndexOf("-")-2);
+                        Console.WriteLine($"Manip: {manip}))");
+                        Console.WriteLine($"Name: {manip.Substring(0, manip.IndexOf("-"))}))");
+                        s.setName(manip.Substring(0, manip.IndexOf("-")));
+                        
+                        manip = manip.Substring(manip.IndexOf("-")+1);
+                        Console.WriteLine($"Manip: {manip}))");
+                        s.setDesc(manip);
+
+                        reply = reply.Substring(reply.IndexOf("|")+1, reply.Length-reply.IndexOf("|")-1);
+                        s.update();
+                        main.addSoldier(s);
+                        count++;
+                    }
+                    Console.WriteLine("Import Complete!");
+                }
                 else
                 {
                     Console.WriteLine("Sorry, I didn't understand that.");
@@ -50,6 +126,37 @@ namespace ProDerSquads
             }
 
 
+        }
+
+
+        //Type - 0 is Derse, 1 is Prospit
+        //Class - 0 is Conscript, 1 is Basic Infantry
+        //after the first two numbers, then name and description
+        public string export()
+        {
+            string s = "";
+            for (int x = 0; x < squad.Length; x++)
+            {
+                if (squad[x].getSpecies().Equals("Derse"))
+                {
+                    s += "0";
+                }
+                if (squad[x].getSpecies().Equals("Prospit"))
+                {
+                    s += "1";
+                }
+                if (squad[x].getType().Equals("Conscript"))
+                {
+                    s += "0";
+                }
+                if (squad[x].getType().Equals("Basic Infantry"))
+                {
+                    s += "1";
+                }
+                s += $"-{squad[x].getName()}-{squad[x].getDesc()}";
+                s += "|";
+            }
+            return s;
         }
 
         public string view()
@@ -136,7 +243,7 @@ namespace ProDerSquads
 
                     //get parts
                     name = reply.Substring(0, reply.IndexOf(" "));
-                    desc = reply.Substring(reply.IndexOf(" "), reply.Length - reply.IndexOf(" "));
+                    desc = reply.Substring(reply.IndexOf(" ")+1, reply.Length - reply.IndexOf(" ")-1);
                     //assign parts
                     s.nameSoldier(name, desc);
                     addSoldier(s);
@@ -223,9 +330,33 @@ namespace ProDerSquads
             String s = "";
             String species = prospit ? "Prospit" : "Derse";
 
-            s += ($"{species} {name}\n\t{description}\n\t{HP}, {wounded}\n\t{DMG}");
+            s += ($"{species} {type} {name}\n\t{description}\n\tHP (wounded): {HP}, {wounded}\n\tDMG: {DMG}");
 
             return s;
+        }
+
+        public void update()
+        {
+            string s = type;
+            //conscript
+            if (s.Equals("conscript") || s.Equals("Conscript") || s.Equals("con") || s.Equals("c") || s.Equals("Con"))
+            {
+                DMG = 0.5f;
+                HP = 0.5f;
+                wounded = false;
+                type = "Conscript";
+            }
+            //basic infantry
+            if (s.Equals("basic") || s.Equals("Basic Infantry") || s.Equals("bi") || s.Equals("basic infantry") || s.Equals("infantry"))
+            {
+                DMG = 1;
+                HP = 1;
+                wounded = false;
+                type = "Basic Infantry";
+            }
+
+            s = prospit ? "Prospit" : "Derse";
+            setSpecies(s);
         }
 
         public float getDMG()
@@ -233,9 +364,19 @@ namespace ProDerSquads
             return DMG;
         }
 
+        public void setDMG(float i)
+        {
+            DMG = i;
+        }
+
         public string getName()
         {
             return name;
+        }
+
+        public void setName(string s)
+        {
+            name = s;
         }
 
         public string getType()
@@ -243,15 +384,51 @@ namespace ProDerSquads
             return type;
         }
 
+        public void setType(string s)
+        {
+            type = s;
+        }
+
         public bool getWounded()
         {
             return wounded;
+        }
+
+        public void setWounded (bool b)
+        {
+            wounded = b;
         }
 
         public string getSpecies()
         {
             String species = prospit ? "Prospit" : "Derse";
             return species;
+        }
+
+        public void setSpecies(string s)
+        {
+            if (s.Equals("Prospit"))
+            {
+                prospit = true;
+            }
+            else if (s.Equals("Derse"))
+            {
+                prospit = false;
+            }
+            else
+            {
+                Console.WriteLine("ERR IN SET SPECIES - SOLDIER");
+            }
+        }
+
+        public string getDesc()
+        {
+            return description;
+        }
+
+        public void setDesc(string s)
+        {
+            description = s;
         }
     }
 
